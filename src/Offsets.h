bdadf2666c45ee15983f77a4a382bc769620aa11
ForestJ316 <91183260+ForestJ316@users.noreply.github.com>
2024-09-23 10:23:45 +0100

    Update to Version 2.1.2

--- src/Offsets.h
+++ src/Offsets.h
@@ -2,5 +2,5 @@
 
 static float* g_deltaTimeRealTime = (float*)RELOCATION_ID(523661, 410200).address();
 
-typedef void(_fastcall* tSetLookAt)(RE::BSScript::Internal::VirtualMachine* a_vm, RE::VMStackID a_stackID, RE::TESObjectREFR* a_source, RE::TESObjectREFR* a_target, bool should_face);
-static REL::Relocation<tSetLookAt> SetLookAt{ RELOCATION_ID(53930, 54754) };
+//typedef void(_fastcall* tSetLookAt)(RE::BSScript::Internal::VirtualMachine* a_vm, RE::VMStackID a_stackID, RE::TESObjectREFR* a_source, RE::TESObjectREFR* a_target, bool should_face);
+//static REL::Relocation<tSetLookAt> SetLookAt{ RELOCATION_ID(53930, 54754) };
