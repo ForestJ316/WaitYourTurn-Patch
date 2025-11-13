1a9f558741412a791d02cb0afb75ddb2f1bd511d
ForestJ316 <91183260+ForestJ316@users.noreply.github.com>
2025-11-13 15:46:26 +0000

    Update Version 2.1.5

--- src/BlockHandler.cpp
+++ src/BlockHandler.cpp
@@ -5,42 +5,69 @@ namespace EnemyHandler
 {
 	void BlockHandler::InitializeForms()
 	{
-		const auto dataHandler = RE::TESDataHandler::GetSingleton();
 		logger::info("Initializing BlockHandler Forms...");
-
-		// BlockingStart Form
-		regularBlockStart_idleForm = dataHandler->LookupForm<RE::TESIdleForm>(0x13217, "Skyrim.esm");
-		if (!regularBlockStart_idleForm)
-			logger::critical("Failed to initialize Form {} (BlockingStart)", regularBlockStart_idleForm->GetFormID());
-
-		// DraugrBlockStart Form (Shields only)
-		draugrBlockStart_idleForm = dataHandler->LookupForm<RE::TESIdleForm>(0x3533C, "Skyrim.esm");
-		if (!draugrBlockStart_idleForm)
-			logger::critical("Failed to initialize Form {} (DraugrBlockStart)", draugrBlockStart_idleForm->GetFormID());
-
-		// FalmerBlockStart Form
-		falmerBlockStart_idleForm = dataHandler->LookupForm<RE::TESIdleForm>(0x36A77, "Skyrim.esm");
-		if (!falmerBlockStart_idleForm)
-			logger::critical("Failed to initialize Form {} (FalmerBlockStart)", falmerBlockStart_idleForm->GetFormID());
-
-		// IdleStop_Loose Form
-		idleStopLoose_idleForm = dataHandler->LookupForm<RE::TESIdleForm>(0x10D9EE, "Skyrim.esm");
-		if (!idleStopLoose_idleForm)
-			logger::critical("Failed to initialize Form {} (IdleStop_Loose)", idleStopLoose_idleForm->GetFormID());
-
-		// Draugr Faction Form
-		draugrFaction = dataHandler->LookupForm<RE::TESFaction>(0x2430D, "Skyrim.esm");
-		if (!draugrFaction)
-			logger::critical("Failed to initialize Form {} (Draugr Faction)", draugrFaction->GetFormID());
-
-		// Falmer Faction Form
-		falmerFaction = dataHandler->LookupForm<RE::TESFaction>(0x2997E, "Skyrim.esm");
-		if (!falmerFaction)
-			logger::critical("Failed to initialize Form {} (Falmer Faction)", falmerFaction->GetFormID());
-
-		block_spell = dataHandler->LookupForm<RE::SpellItem>(0x804, "WaitYourTurn.esp");
-		if (!block_spell)
-			logger::critical("Failed to initialize Block Spell");
+		const auto dataHandler = RE::TESDataHandler::GetSingleton();
+		if (dataHandler) {
+			// BlockingStart Form
+			const auto regularBlockStart_Form = dataHandler->LookupForm(0x13217, "Skyrim.esm");
+			if (regularBlockStart_Form && regularBlockStart_Form->Is(RE::TESIdleForm::FORMTYPE)) {
+				regularBlockStart_idleForm = regularBlockStart_Form->As<RE::TESIdleForm>();
+			}
+			else {
+				logger::critical("Failed to initialize BlockingStart Form");
+			}
+			// DraugrBlockStart Form (Shields only)
+			const auto draugrBlockStart_Form = dataHandler->LookupForm(0x3533C, "Skyrim.esm");
+			if (draugrBlockStart_Form && draugrBlockStart_Form->Is(RE::TESIdleForm::FORMTYPE)) {
+				draugrBlockStart_idleForm = draugrBlockStart_Form->As<RE::TESIdleForm>();
+			}
+			else {
+				logger::critical("Failed to initialize DraugrBlockStart Form");
+			}
+			// FalmerBlockStart Form
+			const auto falmerBlockStart_Form = dataHandler->LookupForm(0x36A77, "Skyrim.esm");
+			if (falmerBlockStart_Form && falmerBlockStart_Form->Is(RE::TESIdleForm::FORMTYPE)) {
+				falmerBlockStart_idleForm = falmerBlockStart_Form->As<RE::TESIdleForm>();
+			}
+			else {
+				logger::critical("Failed to initialize FalmerBlockStart Form");
+			}
+			// IdleStop_Loose Form
+			const auto idleStopLoose_Form = dataHandler->LookupForm(0x10D9EE, "Skyrim.esm");
+			if (idleStopLoose_Form && idleStopLoose_Form->Is(RE::TESIdleForm::FORMTYPE)) {
+				idleStopLoose_idleForm = idleStopLoose_Form->As<RE::TESIdleForm>();
+			}
+			else {
+				logger::critical("Failed to initialize IdleStop_Loose Form");
+			}
+			// Draugr Faction Form
+			const auto draugrFaction_Form = dataHandler->LookupForm(0x2430D, "Skyrim.esm");
+			if (draugrFaction_Form && draugrFaction_Form->Is(RE::TESFaction::FORMTYPE)) {
+				draugrFaction = draugrFaction_Form->As<RE::TESFaction>();
+			}
+			else {
+				logger::critical("Failed to initialize Draugr Faction Form");
+			}
+			// Falmer Faction Form
+			const auto falmerFaction_Form = dataHandler->LookupForm(0x2997E, "Skyrim.esm");
+			if (falmerFaction_Form && falmerFaction_Form->Is(RE::TESFaction::FORMTYPE)) {
+				falmerFaction = falmerFaction_Form->As<RE::TESFaction>();
+			}
+			else {
+				logger::critical("Failed to initialize Falmer Faction Form");
+			}
+			// Block Spell Form
+			const auto block_spell_Form = dataHandler->LookupForm(0x804, "WaitYourTurn.esp");
+			if (block_spell_Form && block_spell_Form->Is(RE::SpellItem::FORMTYPE)) {
+				block_spell = block_spell_Form->As<RE::SpellItem>();
+			}
+			else {
+				logger::critical("Failed to initialize Block Spell");
+			}
+		}
+		else {
+			logger::critical("TESDataHandler not found.");
+		}
 
 		logger::info("...BlockHandler Forms finished initializing.");
 	}
